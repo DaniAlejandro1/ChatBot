7b3cea3189accc6fa49bf58b99beddca186edc3f
Daniel Alejandro <d.toro05@ufromail.cl>
2025-04-14 23:09:23 -0400

    Fix: paquetes

--- back/src/controller/routes/chat.py
+++ back/src/controller/routes/chat.py
@@ -7,8 +7,6 @@ router = APIRouter()
 async def hola():
     return  "hello world"
 
-@router.get("/name")
-async def saludo():
-    nombre = "hola"
-    print(nombre)
-    return "Hola Jorge"
+@router.post("/name/{name}")
+async def saludo(name):
+    return "Hola " + name

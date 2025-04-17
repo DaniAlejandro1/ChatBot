6c58e1b403e015e69a5c44ff97da1bd8132931c8
Claudio Espinoza <claudioyugen@gmail.com>
2025-04-17 15:28:58 -0400

    feat<frontend>: agrega rutas relativas

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
    plugins: [pluginVue()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
    module: {
       rules: [
          {

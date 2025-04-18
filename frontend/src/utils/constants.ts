e64790684d4dc9c51e4cf2c1843958966840f141
Claudio Espinoza <claudioyugen@gmail.com>
2025-04-18 19:46:55 -0400

    format<frontend>: se estiliza el codigo con prettier

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,27 +2,28 @@ type Topic = {
    img: string;
    title: string;
    description: string;
-}
+};
 
 export const topyc: Topic[] = [
    {
-      img: "cloud.png",
-      title: "Clima",
-      description: "Quiero conocer el clima actual en mi ubicación (Temuco, Chile).",
+      img: 'cloud.png',
+      title: 'Clima',
+      description: 'Quiero conocer el clima actual en mi ubicación (Temuco, Chile).',
    },
    {
-      img: "money-calculator.png",
-      title: "Valor del UF",
-      description: "Quiero saber el valor actual de la UF en pesos chilenos.",
+      img: 'money-calculator.png',
+      title: 'Valor del UF',
+      description: 'Quiero saber el valor actual de la UF en pesos chilenos.',
    },
    {
-      img: "money-alt.png",
-      title: "Valor del dólar",
-      description: "Quiero saber el valor actual del dólar en pesos chilenos.",
+      img: 'money-alt.png',
+      title: 'Valor del dólar',
+      description: 'Quiero saber el valor actual del dólar en pesos chilenos.',
    },
    {
-      img: "news.png",
-      title: "Noticias del dia",
-      description: "Quiero saber las noticias más importantes sobre mi empresa de celulosa.",
+      img: 'news.png',
+      title: 'Noticias del dia',
+      description:
+         'Quiero saber las noticias más importantes sobre mi empresa de celulosa.',
    },
 ];

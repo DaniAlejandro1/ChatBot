566552f56276bbacea4f282ef66dd7d30dd0820c
Claudio Espinoza <claudioyugen@gmail.com>
2025-04-18 19:43:26 -0400

    refactor<frontend>: mueve los iconos desde publico a assets

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,28 @@
+type Topic = {
+   img: string;
+   title: string;
+   description: string;
+}
+
+export const topyc: Topic[] = [
+   {
+      img: "cloud.png",
+      title: "Clima",
+      description: "Quiero conocer el clima actual en mi ubicación (Temuco, Chile).",
+   },
+   {
+      img: "money-calculator.png",
+      title: "Valor del UF",
+      description: "Quiero saber el valor actual de la UF en pesos chilenos.",
+   },
+   {
+      img: "money-alt.png",
+      title: "Valor del dólar",
+      description: "Quiero saber el valor actual del dólar en pesos chilenos.",
+   },
+   {
+      img: "news.png",
+      title: "Noticias del dia",
+      description: "Quiero saber las noticias más importantes sobre mi empresa de celulosa.",
+   },
+];
